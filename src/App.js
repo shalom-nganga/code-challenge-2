780bd2cc965ec2bfd3e9cea22bf440baff450261
shalom-nganga <minnomacharia@gmail.com>
2023-11-19 17:35:52 +0300

    edited the app.js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   const [data, setData]=useState([])
   useEffect(()=>{
-    fetch(`http://localhost:8000/bots`)
+    fetch(` http://localhost:8000/bots`)
     .then((res)=>res.json())
     .then((bots)=>{
       setData(bots)
