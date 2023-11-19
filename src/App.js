f2934e96e8669c49dc3075a62651351c6c4de63c
shalom-nganga <minnomacharia@gmail.com>
2023-11-19 17:04:18 +0300

    edited the app.js file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { useEffect} from 'react';
-import './App.css';
-
+import Bot from './components/Bot';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  useEffect(() => {
-    // Fetch the data from the file
-    fetch('http://localhost:8000/bots') // Replace the URL with the correct path to your db.json file
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
-
-return (
-  <>
-  <div>
-   <ul>
-    <li>
-      {bot.name}
-    </li>
-   </ul>
-  </div>
-  </>
-)
-
+  const [data, setData]=useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:8000/bots`)
+    .then((res)=>res.json())
+    .then((bots)=>{
+      setData(bots)
+    })
+  }, [])
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={data && <Bot bots={data}/>}/>
+          
+        </Routes>
+    </BrowserRouter>
+    </div>
+  );
 }
-  
-        
-  
-
 
 export default App;
